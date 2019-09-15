e56c126eb9c475f8098c934f3805205358788f82
Westonbaur <46613163+Westonbaur@users.noreply.github.com>
2019-09-15 11:30:05 -0700

    Add files via upload

---
+++ Pace_Manual_Source_Code_Week_3.py
@@ -0,0 +1,11 @@
+
+def main():
+
+    x = 0
+
+    while x < 100:
+        x = x + 1
+        print(x)
+
+
+main()
---
+++ Pace_Manual_Source_Code_Week_3_2.py
@@ -0,0 +1,10 @@
+
+def main():
+    print("Counting to 50")
+    x = 0
+
+    while x < 50:
+        x = x + 1
+        print(x)
+
+main()
